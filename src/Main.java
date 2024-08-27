d24c0017498779f8895e34f717f9c32c10959176
anonym1111 <mskon@inbox.ru>
2024-08-27 17:00:01 +0300

    Создал Классы MyArraySizeException and MyArrayDataException.

--- src/Main.java
+++ src/Main.java
@@ -78,6 +78,22 @@ public class Main {
         circle.displayProperties();
         rectangle.displayProperties();
         triangle.displayProperties();
+
+        String[][] testArray = {
+                {"1", "2", "3", "4"},
+                {"5", "6", "7", "8"},
+                {"9", "10", "11", "12"},
+                {"13", "14", "15", "16"}
+        };
+
+        try {
+            int result = processArray(testArray);
+            System.out.println("Сумма элементов массива: " + result);
+        } catch (MyArraySizeException e) {
+            System.out.println("Ошибка размера массива: " + e.getMessage());
+        } catch (MyArrayDataException e) {
+            System.out.println("Ошибка данных массива: " + e.getMessage());
+        }
     }
 
     public static void printFruits() {
@@ -200,6 +216,31 @@ public class Main {
         }
         return array;
     }
+    public static int processArray(String[][] array) throws MyArraySizeException, MyArrayDataException {
+        if (array.length != 4) {
+            throw new MyArraySizeException("Массив должен быть размером 4x4");
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].length != 4) {
+                throw new MyArraySizeException("Массив должен быть размером 4x4");
+            }
+        }
+
+        int sum = 0;
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                try {
+                    sum += Integer.parseInt(array[i][j]);
+                } catch (NumberFormatException e) {
+                    throw new MyArrayDataException("Ошибка в ячейке [" + i + "][" + j + "]: " + array[i][j]);
+                }
+            }
+        }
+
+        return sum;
+    }
     }
 
     class Bowl {
@@ -226,6 +267,7 @@ public class Main {
         public int getFoodAmount() {
             return foodAmount;
         }
+
     }
 
 
